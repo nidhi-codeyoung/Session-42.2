0586504e9b08f99304205fea9fcaf64e7f3e2229
Nidhi Satija <126676344+nidhi-codeyoung@users.noreply.github.com>
2023-03-04 22:04:49 +0530

    Add files via upload

---
+++ Exercise 42.2/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from './Calculator';
+import './App.css';
+
+function App() {
+  return (
+    <><h2>The Bill Calculator App</h2>
+    <Calculator /></>
+    
+  );
+}
+
+export default App;
